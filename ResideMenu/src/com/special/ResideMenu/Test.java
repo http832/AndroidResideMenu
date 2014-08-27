924e3b0dc07c17a075c8dd7e03164e39772599ff
http832 <http832@163.com>
2014-08-27 09:37:31 +0800

    test

---
+++ ResideMenu/src/com/special/ResideMenu/Test.java
@@ -0,0 +1,14 @@
+package com.special.ResideMenu;
+
+/**
+ * <一句话功能简述>
+ * @author  neusoft
+ * @createon 2014年8月27日
+ */
+public class Test {
+    /**
+     * <一句话功能简述>
+     * @author  neusoft
+     * @createon 2014年8月27日
+     */
+}
